0ab9dfed6517ee8837895d4976645c7ab3377262
AdarshTamang2060 <adarshtamang2060@gmail.com>
2022-12-23 12:09:50 +0545

    adarsh

--- database/database.php
+++ database/database.php
@@ -27,7 +27,7 @@
      {
  
          try {
-             $this->_connection = new PDO("mysql:host=localhost;dbname=consultancy","root","");
+             $this->_connection = new PDO("mysql:host=localhost;dbname=consultancys","root","");
              $this->_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
          } catch (PDOException $exception) {
